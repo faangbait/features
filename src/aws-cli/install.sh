3b1706eba09c6c09a911e54459937706fd26db8f
𝚜𝚜@𝚏𝚎𝚍𝚘𝚛𝚊:~/𝚍𝚎𝚟$ █ <101474875+faangbait@users.noreply.github.com>
2026-07-22 01:58:31 +0000

    remove extra stuff

--- src/aws-cli/install.sh
+++ src/aws-cli/install.sh
@@ -60,57 +60,6 @@ install_debian_packages() {
     local package_list=""
     package_list="${package_list} curl ca-certificates gpg dirmngr unzip bash-completion less"
 
-    if [ "${INSTALL_SSL}" = "true" ]; then
-        # Include libssl1.1 if available
-        if [[ ! -z $(apt-cache --names-only search ^libssl1.1$) ]]; then
-            package_list="${package_list} libssl1.1"
-        fi
-
-        # Include libssl3 if available
-        if [[ ! -z $(apt-cache --names-only search ^libssl3$) ]]; then
-            package_list="${package_list} libssl3"
-        fi
-
-        # Include appropriate version of libssl1.0.x if available
-        local libssl_package=$(dpkg-query -f '${db:Status-Abbrev}\t${binary:Package}\n' -W 'libssl1\.0\.?' 2>&1 || echo '')
-        if [ "$(echo "$libssl_package" | grep -o 'libssl1\.0\.[0-9]:' | uniq | sort | wc -l)" -eq 0 ]; then
-            if [[ ! -z $(apt-cache --names-only search ^libssl1.0.2$) ]]; then
-                # Debian 9
-                package_list="${package_list} libssl1.0.2"
-            elif [[ ! -z $(apt-cache --names-only search ^libssl1.0.0$) ]]; then
-                # Ubuntu 18.04
-                package_list="${package_list} libssl1.0.0"
-            fi
-        fi
-    fi
-
-    # Include git if not already installed (may be more recent than distro version)
-    if ! type git > /dev/null 2>&1; then
-        package_list="${package_list} git"
-    fi
-
-    # Needed for adding manpages-posix and manpages-posix-dev which are non-free packages in Debian
-    if [ "${ADD_NON_FREE_PACKAGES}" = "true" ]; then
-        if [[ ! -e "/etc/apt/sources.list" ]] && [[ -e "/etc/apt/sources.list.d/debian.sources" ]]; then 
-            sed -i '/^URIs: http:\/\/deb.debian.org\/debian$/ { N; N; s/Components: main/Components: main non-free non-free-firmware/ }' /etc/apt/sources.list.d/debian.sources
-        else
-            # Bring in variables from /etc/os-release like VERSION_CODENAME
-            sed -i -E "s/deb http:\/\/(deb|httpredir)\.debian\.org\/debian ${VERSION_CODENAME} main/deb http:\/\/\1\.debian\.org\/debian ${VERSION_CODENAME} main contrib non-free/" /etc/apt/sources.list
-            sed -i -E "s/deb-src http:\/\/(deb|httredir)\.debian\.org\/debian ${VERSION_CODENAME} main/deb http:\/\/\1\.debian\.org\/debian ${VERSION_CODENAME} main contrib non-free/" /etc/apt/sources.list
-            sed -i -E "s/deb http:\/\/(deb|httpredir)\.debian\.org\/debian ${VERSION_CODENAME}-updates main/deb http:\/\/\1\.debian\.org\/debian ${VERSION_CODENAME}-updates main contrib non-free/" /etc/apt/sources.list
-            sed -i -E "s/deb-src http:\/\/(deb|httpredir)\.debian\.org\/debian ${VERSION_CODENAME}-updates main/deb http:\/\/\1\.debian\.org\/debian ${VERSION_CODENAME}-updates main contrib non-free/" /etc/apt/sources.list
-            sed -i "s/deb http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}\/updates main/deb http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}\/updates main contrib non-free/" /etc/apt/sources.list
-            sed -i "s/deb-src http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}\/updates main/deb http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}\/updates main contrib non-free/" /etc/apt/sources.list
-            sed -i "s/deb http:\/\/deb\.debian\.org\/debian ${VERSION_CODENAME}-backports main/deb http:\/\/deb\.debian\.org\/debian ${VERSION_CODENAME}-backports main contrib non-free/" /etc/apt/sources.list
-            sed -i "s/deb-src http:\/\/deb\.debian\.org\/debian ${VERSION_CODENAME}-backports main/deb http:\/\/deb\.debian\.org\/debian ${VERSION_CODENAME}-backports main contrib non-free/" /etc/apt/sources.list
-            # Handle bullseye location for security https://www.debian.org/releases/bullseye/amd64/release-notes/ch-information.en.html
-            sed -i "s/deb http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}-security main/deb http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}-security main contrib non-free/" /etc/apt/sources.list
-            sed -i "s/deb-src http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}-security main/deb http:\/\/security\.debian\.org\/debian-security ${VERSION_CODENAME}-security main contrib non-free/" /etc/apt/sources.list
-        fi;
-        echo "Running apt-get update..."
-        package_list="${package_list} manpages-posix manpages-posix-dev"
-    fi
-
     local missing_package_list=""
     local packages=()
     read -r -a packages <<< "${package_list}"
@@ -126,24 +75,6 @@ install_debian_packages() {
         rm -rf /var/lib/apt/lists/*
         apt-get update -y
         apt-get -y install --no-install-recommends ${missing_package_list} 2> >( grep -v 'debconf: delaying package configuration, since apt-utils is not installed' >&2 )
-    fi
-
-    # Install zsh (and recommended packages) if needed
-    if [ "${INSTALL_ZSH}" = "true" ] && ! type zsh > /dev/null 2>&1; then
-        apt-get install -y zsh
-    fi
-
-    # Get to latest versions of all packages
-    if [ "${UPGRADE_PACKAGES}" = "true" ]; then
-        apt-get -y upgrade --no-install-recommends
-        apt-get autoremove -y
-    fi
-
-    # Ensure at least the en_US.UTF-8 UTF-8 locale is available = common need for both applications and things like the agnoster ZSH theme.
-    if [ "${LOCALE_ALREADY_SET}" != "true" ] && ! grep -o -E '^\s*en_US.UTF-8\s+UTF-8' /etc/locale.gen > /dev/null; then
-        echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-        locale-gen
-        LOCALE_ALREADY_SET="true"
     fi
 
     # Clean up
@@ -171,43 +102,6 @@ install_redhat_packages() {
     
     package_list="${package_list} curl ca-certificates gpg dirmngr unzip bash-completion less"
     
-    # Install bubblewrap if available (not present in UBI repositories)
-    if ${install_cmd} -q list bubblewrap >/dev/null 2>&1; then
-        package_list="${package_list} bubblewrap"
-    fi
-
-    # rockylinux:9 installs 'curl-minimal' which clashes with 'curl'
-    # Install 'curl' for every OS except this rockylinux:9
-    if [[ "${ID}" = "rocky" ]] && [[ "${VERSION}" != *"9."* ]]; then
-        package_list="${package_list} curl"
-    fi
-
-    # Install OpenSSL 1.0 compat if needed
-    if ${install_cmd} -q list compat-openssl10 >/dev/null 2>&1; then
-        package_list="${package_list} compat-openssl10"
-    fi
-
-    # Install lsb_release if available
-    if ${install_cmd} -q list redhat-lsb-core >/dev/null 2>&1; then
-        package_list="${package_list} redhat-lsb-core"
-    fi
-
-    # Install git if not already installed (may be more recent than distro version)
-    if ! type git > /dev/null 2>&1; then
-        package_list="${package_list} git"
-    fi
-
-    # Install EPEL repository if needed (required to install 'jq' for CentOS)
-    if [[ "${ID}" = "centos" ]] && ! rpm -q jq >/dev/null 2>&1; then
-        ${install_cmd} -y install epel-release
-        remove_epel="true"
-    fi
-
-    # Install zsh if needed
-    if [ "${INSTALL_ZSH}" = "true" ] && ! type zsh > /dev/null 2>&1; then
-        package_list="${package_list} zsh"
-    fi
-
     local missing_package_list=""
     local packages=()
     read -r -a packages <<< "${package_list}"
@@ -244,29 +138,6 @@ install_alpine_packages() {
     local package_list=""
 
     package_list="${package_list} curl ca-certificates gnupg unzip bash-completion less"
-
-    # # Include libssl1.1 if available (not available for 3.19 and newer)
-    LIBSSL1_PKG=libssl1.1
-    if [[ $(apk search --no-cache -a $LIBSSL1_PKG | grep $LIBSSL1_PKG) ]]; then
-        package_list="${package_list} $LIBSSL1_PKG"
-    fi
-
-    # Install man pages - package name varies between 3.12 and earlier versions
-    if apk info man > /dev/null 2>&1; then
-        package_list="${package_list} man man-pages"
-    else
-        package_list="${package_list} mandoc man-pages"
-    fi
-
-    # Install git if not already installed (may be more recent than distro version)
-    if ! type git > /dev/null 2>&1; then
-        package_list="${package_list} git"
-    fi
-
-    # Install zsh if needed
-    if [ "${INSTALL_ZSH}" = "true" ] && ! type zsh > /dev/null 2>&1; then
-        package_list="${package_list} zsh"
-    fi
 
     local missing_package_list=""
     local packages=()
